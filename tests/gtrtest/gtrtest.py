c2d80767ef6cd94937202a24159fb3966721dcfb
Paul O. Lewis <paul.lewis@uconn.edu>
2015-08-30 13:02:19 -0400

    modified gtrtest

--- tests/gtrtest/gtrtest.py
+++ tests/gtrtest/gtrtest.py
@@ -12,6 +12,8 @@ model.num_rates = 4
 model.gamma_shape = 0.5
 model.pinvar_model = True
 
+model.state_freqs = [.1,.2,.3,.4,.5]
+
 model.edgelen_prior = Exponential(1.0)
 
 mcmc.out.log = 'output.txt'
